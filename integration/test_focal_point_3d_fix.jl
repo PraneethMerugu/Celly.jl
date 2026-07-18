875ba3ebb28751ca053a48621dc3e02e3c240aeb
PraneethMerugu <merugufam@gmail.com>
2026-07-17 20:54:00 -0400

    Fix 3D focal-point integration fixture

--- integration/test_focal_point_3d_fix.jl
+++ integration/test_focal_point_3d_fix.jl
@@ -7,7 +7,7 @@ using Test
     grid[6, 5, 5] = 2
 
     # 1 and 2 are connected
-    conn = [0 0 0; 0 0 1; 0 1 0]
+    conn = [0 1; 1 0]
     lambdas = Float32[0.0f0, 100.0f0, 100.0f0]
     target_lengths = Float32[0.0f0, 5.0f0, 5.0f0]
     fp_pen = FocalPointSpringPenalty(lambdas, target_lengths, conn)
@@ -28,6 +28,15 @@ using Test
     prob = PottsProblem(sys, (0, 100), params)
     sol = CorePotts.SciMLBase.solve(prob, SequentialMetropolis())
 
-    # Run some sweeps and ensure they don't NaN or diverge
-    @test !isnan(sol.u[end].cell_data.center_x[1])
+    # Run some sweeps and ensure the 3D anchors and forces remain finite.
+    final_cell_data = sol.u[end].cell_data
+    mechanical_state = (
+        final_cell_data.anchor_x[1:2]...,
+        final_cell_data.anchor_y[1:2]...,
+        final_cell_data.anchor_z[1:2]...,
+        final_cell_data.force_x[1:2]...,
+        final_cell_data.force_y[1:2]...,
+        final_cell_data.force_z[1:2]...,
+    )
+    @test all(isfinite, mechanical_state)
 end
